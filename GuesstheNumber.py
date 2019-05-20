ac03e629e57ad59cecb716f0338032316a0c6af3
CHigley93 <50812667+CHigley93@users.noreply.github.com>
2019-05-19 20:08:05 -0500

    Add files via upload

--- GuesstheNumber.py
+++ GuesstheNumber.py
@@ -2,11 +2,13 @@
 
 import random
 mysteryNumber = random.randrange(1,21,1)
+numberGuess = int(input('I am thinking of a number, can you guess it? It is between 1 and 20 '))
+
 while True:
-    numberGuess = int(input('I am thinking of a number, can you guess it? It is between 1 and 20 '))
     while numberGuess > mysteryNumber:
         numberGuess = int(input('that number is too high, guess again! '))
     while numberGuess < mysteryNumber:
         numberGuess = int(input('that number is too low, guess again! '))
-    print('You guessed the number {} is what I was thinking of! Let\'s play again, I am thinking of a new number.'.format(mysteryNumber))
-    mysteryNumber = random.randrange(1,21,1)
+    while numberGuess == mysteryNumber:
+        numberGuess = int(input('You guessed the number! {} is what I was thinking of! Let\'s play again, I am thinking of a new number.'.format(mysteryNumber)))
+        mysteryNumber = random.randrange(1,21,1)
